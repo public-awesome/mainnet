51d06a350da120ec72e163f2785152994c9b1b4f
jhernandezb <contact@jhernandez.me>
2021-10-26 00:59:41 -0600

    fix check gentx

--- ci/check-gentx.sh
+++ ci/check-gentx.sh
@@ -7,6 +7,6 @@ if [ -f "$FILE" ]; then
     mkdir -p ~/.starsd/config/gentx
     cp "./$FILE_LOCATION" ~/.starsd/config/gentx/
     export ADDRESS=$(cat check-gen-tx)
-    starsd add-genesis-account $ADDRESS 10000000000stake
+    starsd add-genesis-account $ADDRESS 10000000000ustars
     starsd collect-gentxs
 fi
